875f96583191c1d19e141faca86e617b24d02754
alexanderstog <alexanderstogdon@gmail.com>
2019-05-01 17:31:35 +0100

    fact slider
    fact slider

--- app.js
+++ app.js
@@ -21,7 +21,25 @@ var nav = [{
 
 
 
-
+var facts = [{
+    Title:'Some people eat it raw',
+    Info:'Some people do not need to wait for the five minutes to prepare ramen noodles. They just sprinkle the raw, crunchy and hard noodles with the seasoning and dig in. This might seem weird but many people do it as a quick snack.'
+},  {
+    Title:'First of its kind to be eaten in space',
+    Info:'The inventor of instant noodles, Momofuku Ando never would have dreamed to have his invention be eaten in space as it happened back in 2005.  He actually invented a better vacuum sealed package version of noodles shortly before his death.  This version had smaller noodles and a thicker broth to make it easy to eat in zero gravity conditions. Japanese astronaut, Soichi Noguchi carried some Space Ram Noodles to space with him in the Discovery space shuttle.'
+},  {
+    Title:'You only need $150 only to survive on ramen for a year',
+    Info:'The thing that makes ramen so popular is the price. For just 14 cents you can get a meal easy enough to prepare in ten minutes or less. If you do the math, it would cost you a little over $150 if you lived off ramen noodles for a whole year. This can have you saving thousands of dollars since on average, one American spends over $7000 on food.'
+},  {
+    Title:'When they were first invented, were considered luxurious',
+    Info:'When Momokufu Ando first released Chicken Ramen to the Japanese supermarkets in 1958, it was six times more expensive than fresh udon noodles. This made ramen noodles unaffordable to the vast majority of poor Japanese people. '
+},  {
+    Title:'There is a museum dedicated to noodles',
+    Info:'The Japanese love their noodles so much they have dedicated a whole museum to showcase the history of ramen noodles and the story of how Momofuku Ando created one of Japan\'s finest inventions. The museum has a model factory where guests can make unique ramen concoctions using either of the available 5,460 flavor combinations.'
+}
+    
+    
+];
 
 
 /*
@@ -84,10 +102,11 @@ app.get('/', function(req,res){
     // "I Am Kloot &mdash; Loch",
     // "Portishead &mdash; Glory Box"]
         res.render('index', {
-       title:'BananaRamen', 
-       metadescription:'Ramen',
+       title:'Ramen-Drop', 
+       description:'Vote for your campus to receive a boat load of Ramen!',
        nav: nav,
-       chart: chart
+       chart: chart,
+       facts:facts
    }); 
 });
    
